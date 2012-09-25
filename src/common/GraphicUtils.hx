4dfeb476ca3fe55d853aa48b169056225781db87
soywiz <soywiz@gmail.com>
2012-09-25 16:05:37 +0200

    - Added support for Sprite9Slice

--- src/common/GraphicUtils.hx
+++ src/common/GraphicUtils.hx
@@ -2,7 +2,10 @@ package common;
 import nme.display.Bitmap;
 import nme.display.BitmapData;
 import nme.display.Graphics;
+import nme.display.Sprite;
 import nme.geom.Matrix;
+import nme.geom.Point;
+import nme.Vector;
 
 /**
  * ...
@@ -13,13 +16,22 @@ class GraphicUtils
 {
 	static private var matrix:Matrix = new Matrix();
 
-	static public function drawBitmapSlice(graphics:Graphics, bitmap:BitmapData, x:Int, y:Int, srcX:Int, srcY:Int, w:Int, h:Int):Void {
-		matrix.identity();
-		matrix.translate(x - srcX, y - srcY);
-		graphics.beginBitmapFill(bitmap, matrix, false, true);
-		{
-			graphics.drawRect(x, y, w, h);
-		}
+	static public function drawBitmapSlice(graphics:Graphics, bitmapData:BitmapData, dstX:Int, dstY:Int, srcX:Int, srcY:Int, dstW:Int, dstH:Int, ?srcW:Int, ?srcH:Int):Void {
+		if (srcW == null) srcW = dstW;
+		if (srcH == null) srcH = dstH;
+
+		var pointTL:Point = new Point(dstX       , dstY       );
+		var pointBR:Point = new Point(dstX + dstW, dstY + dstH);
+
+		var uvPointTL:Point = new Point((srcX) / bitmapData.width, (srcY) / bitmapData.height);
+		var uvPointBR:Point = new Point((srcX + srcW) / bitmapData.width, (srcY + srcH) / bitmapData.height);
+		
+		var verticies:Array<Float> = [pointTL.x, pointTL.y, pointBR.x, pointTL.y, pointTL.x, pointBR.y, pointBR.x, pointBR.y];
+		var uvtData:Array<Float> = [uvPointTL.x, uvPointTL.y, uvPointBR.x, uvPointTL.y, uvPointTL.x, uvPointBR.y, uvPointBR.x, uvPointBR.y];
+		var indices:Array<Int> = [0, 1, 2, 1, 3, 2];
+		 
+		graphics.beginBitmapFill(bitmapData, null, false, true);
+		graphics.drawTriangles(verticies, indices, uvtData);
 		graphics.endFill();
 	}
 
---
+++ src/common/display/Sprite9Slice.hx
@@ -0,0 +1,76 @@
+package common.display;
+import common.GraphicUtils;
+import common.SpriteUtils;
+import nme.display.BitmapData;
+import nme.display.Sprite;
+import nme.geom.Rectangle;
+
+/**
+ * ...
+ * @author soywiz
+ */
+
+class Sprite9Slice extends Sprite
+{
+	var bitmapData:BitmapData;
+	var innerRectangle:Rectangle;
+
+	public function new(bitmapData:BitmapData, innerRectangle:Rectangle) 
+	{
+		super();
+		
+		this.bitmapData = bitmapData;
+		this.innerRectangle = innerRectangle;
+	}
+	
+	public function update(width:Int, height:Int):Void
+	{
+		SpriteUtils.removeSpriteChilds(this);
+		
+		var widthLeft:Int = Std.int(innerRectangle.left), widthRight:Int = Std.int(bitmapData.width - innerRectangle.right);
+		var heightTop:Int = Std.int(innerRectangle.top), heightBottom:Int = Std.int(bitmapData.height - innerRectangle.bottom);
+
+		var drawWidths:Array<Int> = [widthLeft, width - widthLeft - widthRight, widthRight];
+		var drawHeights:Array<Int> = [heightTop, height - heightTop - heightBottom, heightBottom];
+		
+		var bitmapWidths:Array<Int> = [widthLeft, bitmapData.width - widthLeft - widthRight, widthRight];
+		var bitmapHeights:Array<Int> = [heightTop, bitmapData.height - heightTop - heightBottom, heightBottom];
+		
+		//trace(width + ", " + height);
+		//trace(drawWidths + ", " + drawHeights);
+
+		//trace(bitmapData.width + ", " + bitmapData.height);
+		//trace(bitmapWidths + ", " + bitmapHeights);
+
+		//var xList:Array<Int> = [0, widthLeft];
+		
+		var bitmapY:Int = 0;
+		var drawY:Int = 0;
+		for (hIndex in 0 ... 3) {
+			var bitmapHeight:Int = bitmapHeights[hIndex];
+			var drawHeight:Int = drawHeights[hIndex];
+
+			var bitmapX:Int = 0;
+			var drawX:Int = 0;
+			for (wIndex in 0 ... 3) {
+				var bitmapWidth:Int = bitmapWidths[wIndex];
+				var drawWidth:Int = drawWidths[wIndex];
+				
+				//trace(Std.format("($drawX, $drawY)-($drawWidth, $drawHeight) : ($bitmapX, $bitmapY)-($bitmapWidth, $bitmapHeight)"));
+
+				GraphicUtils.drawBitmapSlice(
+					graphics, bitmapData,
+					drawX, drawY,
+					bitmapX, bitmapY,
+					drawWidth, drawHeight,
+					bitmapWidth, bitmapHeight
+				);
+				
+				bitmapX += bitmapWidth;
+				drawX += drawWidth;
+			}
+			bitmapY += bitmapHeight;
+			drawY += drawHeight;
+		}
+	}
+}
